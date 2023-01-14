762c65044ebf0e45bca4f21696143787156c767e
Bradley <44791542+hazard-10@users.noreply.github.com>
2023-01-14 12:18:32 -0500

    update minor details

---
+++ Assets/Oasis/Scripts/PrintControllerLocation.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PrintControllerLocation : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] GameObject ctrl;
+    Text text;
+
+    void Start()
+    {
+        text = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = ctrl.transform.position.ToString() + " ceck";
+    }
+}
